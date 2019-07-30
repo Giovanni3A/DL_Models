be91c46c83b66ef2b6806f172ab0a015c6e34de5
giovanniamorim <giovanniamorim@psr-inc.com>
2019-07-30 00:59:38 -0300

    adds multi classification model

--- MCNN_model.py
+++ MCNN_model.py
@@ -20,7 +20,8 @@ ntrn      = trnfil_pd.shape[0]
 img_dtsz  = len(list(Image.open(os.path.join(TRAIN_PATH,trnfil_pd.filename[0])).convert('LA').getdata(0)))
 row_len   = int(img_dtsz**(1/2))
 
-y_train = np.zeros(shape=(ntrn,10))
+# Multiple y_trains, 1 for each possible
+classifications = [np.zeros(shape=(ntrn,1)) for i in range(10)]
 X_train = np.zeros(shape=(ntrn, img_dtsz))
 
 print("Loading training images...")
@@ -30,13 +31,13 @@ for i in progressbar(range(ntrn)):
     label    = trnfil_pd.label[i]
     img_data = np.array(Image.open(img_file).convert('LA').getdata(0))
 
-    X_train[i,:]     = img_data/255
-    y_train[i,label] = 1
+    X_train[i,:]              = img_data/255
+    classifications[label][i] = 1
 
 X_train = X_train.reshape(ntrn,1,row_len,row_len)
-X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.33, random_state=666)
 
 def CNN_network(X,y,row_len):
+    
     model = Sequential()
     model.add(Conv2D(64, (7,7), input_shape=(1,row_len,row_len), activation='tanh', use_bias=True, kernel_initializer='normal', data_format='channels_first'))
     model.add(AveragePooling2D(pool_size=(2,2)))
@@ -44,14 +45,32 @@ def CNN_network(X,y,row_len):
     model.add(MaxPooling2D(pool_size=(2,2)))
     model.add(Flatten())
     model.add(Dense(16, activation='tanh', use_bias=True, kernel_initializer='normal'))
-    model.add(Dense(10, activation='softmax', use_bias=True, kernel_initializer='normal'))
+    model.add(Dense(1, activation='tanh', use_bias=True, kernel_initializer='normal'))
 
-    model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+    model.compile(loss='mean_squared_error',optimizer='adam',metrics=['accuracy'])
 
-    model.fit(X,y,batch_size=200,epochs=10,verbose=1)
+    model.fit(X,y,batch_size=200,epochs=2,verbose=1)
+
     return model
 
-model = CNN_network(X_train,y_train,row_len)
+def classifier_predict(models,X):
+    predictions = []
+    results     = [model.predict(X) for model in models]
+    for ix in range(X.shape[0]):
+        ey = [results[m][ix] for m in range(len(models))]
+        predictions.append(ey.index(max(ey)))
+    return predictions
 
-scores = model.evaluate(X_test, y_test, verbose=0)
-print("MLP Model Error: {}".format(round(100-scores[1]*100),2))
+def classifier_evaluate(models,X,y):
+    total_errors = 0
+    total_n      = y.shape[0]
+    predictions  = classifier_predict(models,X)
+    for i in range(len(predictions)):
+        if predictions[i] != y[i]:
+            total_errors += 1
+    return total_errors/total_n
+
+models = [CNN_network(X_train,y_train,row_len) for y_train in classifications]
+
+scores = classifier_evaluate(models, X_train, trnfil_pd.label)
+print("Model Error: {}%".format(100*round(scores,3)))
